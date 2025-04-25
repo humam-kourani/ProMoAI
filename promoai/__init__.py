ddcecf05b46ae6fceab2c00c903a62d5bb3219e6
humam-kourani <75330540+humam-kourani@users.noreply.github.com>
2025-04-25 12:43:37 +0200

    upgrade to 1.1.3

--- promoai/__init__.py
+++ promoai/__init__.py
@@ -3,4 +3,4 @@ from promoai.general_utils.app_utils import InputType
 from promoai.main import generate_model_from_event_log, generate_model_from_text, generate_model_from_petri_net, generate_model_from_bpmn
 
 __name__ = 'promoai'
-__version__ = '1.1.2'
+__version__ = '1.1.3'
